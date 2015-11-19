b89af846099e8cc2c3a87fc2ce2dd0b3c8f84208
ralam <rehan.alam@gmail.com>
2015-11-19 13:26:43 -0800

    add key listener with note map

---
+++ app/assets/javascripts/util/key_listener.js
@@ -0,0 +1,23 @@
+(function(root){
+  var NOTE_MAP = {
+    65: 'C4',  // 'a'
+    87: 'C4S', // 'w'
+    83: 'D4',  // 's'
+    69: 'D4S', // 'e'
+    68: 'E4',  // 'd'
+    74: 'F4',  // 'j'
+    73: 'F4S', // 'i'
+    75: 'G4',  // 'k'
+    79: 'G4S', // 'o'
+    76: 'A4',  // 'l'
+    80: 'A4S', // 'p'
+    186: 'B4', // ';'
+  };
+  var heldKeys = [];
+  var validKeys = Object.keys(NOTE_MAP);
+  
+  $(document).on('keyup', function (event) {
+    var keycode = e.keyCode;
+    if
+  })
+})();
